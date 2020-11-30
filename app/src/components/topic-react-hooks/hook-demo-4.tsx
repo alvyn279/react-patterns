cbe5efd8e0ab2b0965a457db7526b8caafe0d46d
alvyn le <alvyn279@gmail.com>
2020-11-30 00:39:36 -0500

    typed response from the api

--- app/src/components/topic-react-hooks/hook-demo-4.tsx
+++ app/src/components/topic-react-hooks/hook-demo-4.tsx
@@ -44,7 +44,7 @@ const useHackerNewsApi = <T, >(initialQuery: string, initialData: T) => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await axios.get(url);
+        const response: { data: T } = await axios.get(url);
         setData(response.data);
         setError(null);
       } catch (err) {
